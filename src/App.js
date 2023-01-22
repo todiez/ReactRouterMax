7671b88d6677269945c54d391d3b01e95cb0fa77
tybo-dev <tobias@diez.guru>
2023-01-22 14:58:16 +0100

    final updates

--- src/App.js
+++ src/App.js
@@ -11,10 +11,12 @@ import {
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Faq from "./pages/help/Faq";
-import Contact from "./pages/help/Contact";
+import Contact, { contactAction } from "./pages/help/Contact";
 import NotFound from "./pages/NotFound";
 import Careers, { careersLoader } from "./pages/careers/Careers";
-import CareerDetails, { careerDetailsLoader } from "./pages/careers/CareerDetails";
+import CareerDetails, {
+  careerDetailsLoader,
+} from "./pages/careers/CareerDetails";
 import CareersError from "./pages/careers/CarreersError";
 
 //layouts import
@@ -29,13 +31,20 @@ const router = createBrowserRouter(
       <Route path="about" element={<About />} />
       <Route path="help" element={<HelpLayout />}>
         <Route path="faq" element={<Faq />} />
-        <Route path="contact" element={<Contact />} />
+        <Route path="contact" element={<Contact />} action={contactAction} />
       </Route>
-      <Route path="careers" element={<CareersLayout />}>
+      <Route
+        path="careers"
+        element={<CareersLayout />}
+        errorElement={<CareersError />}
+      >
         <Route index element={<Careers />} loader={careersLoader} />
-        <Route path=":id" element={<CareerDetails />} loader={careerDetailsLoader} errorElement={<CareersError />} />
+        <Route
+          path=":id"
+          element={<CareerDetails />}
+          loader={careerDetailsLoader}
+        />
       </Route>
-      
       <Route path="*" element={<NotFound />} />
     </Route>
   )
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,22 @@
+import { Navigate } from "react-router-dom";
+import { useState } from "react";
+
 export default function About() {
+
+    const [user, setUser] = useState("paz");
+
+    if(!user) {
+        return <Navigate to="/" replace={true}/>
+    }
+
     return (
       <div className="about">
         <h2>About Us</h2>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui provident consequuntur vel omnis quisquam rem harum, maxime expedita, ullam ut dolore! Distinctio eos minima voluptatum totam id hic! Sapiente debitis quia illum officia obcaecati provident nulla odio molestiae suscipit quasi.</p>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui provident consequuntur vel omnis quisquam rem harum, maxime expedita, ullam ut dolore! Distinctio eos minima voluptatum totam id hic! Sapiente debitis quia illum officia obcaecati provident nulla odio molestiae suscipit quasi.</p>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui provident consequuntur vel omnis quisquam rem harum, maxime expedita, ullam ut dolore! Distinctio eos minima voluptatum totam id hic! Sapiente debitis quia illum officia obcaecati provident nulla odio molestiae suscipit quasi.</p>
+
+        <button onClick={() => setUser(null)}>Logout</button>
       </div>
     )
   }
