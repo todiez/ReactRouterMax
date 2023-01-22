236a55bfe6add0089bea9982278e83d94411b0c8
tybo-dev <tobias@diez.guru>
2023-01-22 12:07:47 +0100

    rootlayout comp added

--- src/App.js
+++ src/App.js
@@ -1,30 +1,39 @@
-import {BrowserRouter, Routes, Route, Link, NavLink} from 'react-router-dom'
+import {
+  createBrowserRouter,
+  BrowserRouter,
+  Route,
+  NavLink,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
 
 //pages import
-import Home from './pages/Home';
-import About from './pages/About';
+import Home from "./pages/Home";
+import About from "./pages/About";
+import RootLayout from "./layouts/RootLayout";
 
-
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={RootLayout}>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Route>
+  )
+);
 
 function App() {
   return (
-    <BrowserRouter>
-    <header>
-      <nav>
-        <h1>JobaRouter</h1>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="about">About</NavLink>
-      </nav>
-    </header>
-    <main>
-      <Routes>
-        <Route path="/" element={<Home />}/>  
-        <Route path="/about" element={<About />}/>
-      </Routes>
-    </main>
-
-    </BrowserRouter>
+    <>
+      {/* <header>
+        <nav>
+          <h1>JobaRouter</h1>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="about">About</NavLink>
+        </nav>
+      </header> */}
+      <RouterProvider router={router} />
+    </>
   );
 }
 
-export default App
+export default App;
