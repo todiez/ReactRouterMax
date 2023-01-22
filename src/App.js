43004d22bd209308b9717cb0f73ed1284119c3ce
tybo-dev <tobias@diez.guru>
2023-01-22 11:32:24 +0100

    repo initialized

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
+
+function App() {
+  return (
+    <div className="App">
+      Hello, Ninjas!
+    </div>
+  );
+}
+
+export default App
